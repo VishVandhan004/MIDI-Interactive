925ad8efa0aca494192494a871cabcb648b5dda0
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-04-27 22:15:36 +0530

    changes

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-
-from flask import Flask, render_template
-import threading
-import midi_logic  # notice: underscore instead of dash
+from flask import Flask, render_template, request
+import midi_logic
 
 app = Flask(__name__)
 
+# serve your piano UI
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def run_midi_logic():
-    midi_logic.start_midi_listener()
+# play a note
+@app.route('/play_note/<note>', methods=['GET'])
+def play_note_route(note):
+    midi_logic.play_note(note)
+    return '', 204
+
+# stop a note
+@app.route('/stop_note/<note>', methods=['GET'])
+def stop_note_route(note):
+    midi_logic.stop_note(note)
+    return '', 204
+
+# change instrument by name
+@app.route('/set_instrument', methods=['POST'])
+def set_instrument():
+    data = request.json or {}
+    name = data.get('instrument')
+    if name:
+        midi_logic.set_program_by_name(name)
+    return '', 204
 
 if __name__ == '__main__':
-    threading.Thread(target=run_midi_logic, daemon=True).start()
-    app.run(host="0.0.0.0", port=10000)
+    # listen on all interfaces so Render can route traffic
+    app.run(host='0.0.0.0', port=10000, debug=True)
